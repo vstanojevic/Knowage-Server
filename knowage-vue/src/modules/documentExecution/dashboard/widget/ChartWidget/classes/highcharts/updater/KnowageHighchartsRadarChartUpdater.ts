f3f37fa638a1714d72b78329ae04c7cd15bbd66f
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-04-26 11:21:26 +0200

    Backward radar

--- knowage-vue/src/modules/documentExecution/dashboard/widget/ChartWidget/classes/highcharts/updater/KnowageHighchartsRadarChartUpdater.ts
+++ knowage-vue/src/modules/documentExecution/dashboard/widget/ChartWidget/classes/highcharts/updater/KnowageHighchartsRadarChartUpdater.ts
@@ -1,6 +1,7 @@
 import { IHighchartsChartModel } from '@/modules/documentExecution/dashboard/interfaces/highcharts/DashboardHighchartsWidget'
 import { getFormattedLegend, getFormattedNoDataConfiguration, getFormattedSeries, setFormattedAxisLabels, setFormattedAxisTitle } from './KnowageHighchartsCommonUpdater'
 import * as highchartsDefaultValues from '../../../../WidgetEditor/helpers/chartWidget/highcharts/HighchartsDefaultValues'
+import { hexToRgba } from '@/modules/documentExecution/dashboard/helpers/FormattingHelpers'
 
 export const updateRadarChartModel = (oldModel: any, newModel: IHighchartsChartModel) => {
     console.log('   ----- OLD CHART MODEL: ', oldModel)
@@ -21,6 +22,7 @@ const getFormattedAxisSettings = (oldModel: any, newModel: IHighchartsChartModel
     if (!oldAxis) return
     setFormattedAxisLabels(oldAxis, newModelAxis)
     setFormattedAxisTitle(oldAxis, newModelAxis)
+    if (axis === 'y') setYAxisGridSettings(oldAxis, newModelAxis)
 
     axis === 'x' ? setXAxisSpecificValues(newModelAxis) : setYAxisSpecificValues(newModelAxis)
     axis === 'x' ? newModel.xAxis = newModelAxis : newModel.yAxis = newModelAxis
@@ -33,4 +35,32 @@ const setXAxisSpecificValues = (newModelAxis: any) => {
 const setYAxisSpecificValues = (newModelAxis: any) => {
     newModelAxis.plotBands = []
     newModelAxis.plotLines = []
+}
+
+
+const setYAxisGridSettings = (oldAxis: any, newModelAxis: any) => {
+    if (oldAxis.MAJORGRID) {
+        newModelAxis.tickInterval = oldAxis.MAJORGRID.interval ?? null
+        newModelAxis.gridLineDashStyle = getFormattedGridLineStyle(oldAxis.MINORGRID.style?.typeline) ? '' : ''
+        newModelAxis.gridLineColor = oldAxis.MAJORGRID.style?.color ? hexToRgba(oldAxis.MAJORGRID.style.color) : ''
+    }
+
+    if (oldAxis.MINORGRID) {
+        newModelAxis.minorTickInterval = oldAxis.MINORGRID.interval ?? null
+        newModelAxis.minorGridLineDashStyle = getFormattedGridLineStyle(oldAxis.MINORGRID.style?.typeline) ? '' : ''
+        newModelAxis.minorGridLineColor = oldAxis.MINORGRID.style?.color ? hexToRgba(oldAxis.MINORGRID.style.color) : ''
+    }
+}
+
+const getFormattedGridLineStyle = (type: string) => {
+    switch (type) {
+        case 'solid':
+            return 'Solid';
+        case "dashed":
+            return 'Dash'
+        case "dotted":
+            return 'Dot';
+        default:
+            return ''
+    }
 }
